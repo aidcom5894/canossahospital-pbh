97f4e6fb3a978b5366b90ec95ce93d4e99ff445b
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2026-06-27 14:41:53 +0530

    updated new configuration file for production

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 
 	$hostname = 'localhost';
-	$username = 'root';
-	$password = 'Admin1234#@';
-	$db_name = 'clinic';
+	$username = 'u445536153_leenaCanossa';
+	$password = 'E9s#|rd*5Iv3:n4O8Z>QEb*pN|l';
+	$db_name = 'u445536153_canossa_pbh';
 
 	$conn = mysqli_connect($hostname,$username,$password,$db_name);
 
 	//date_default_timezone_set('Aisa/Kolkata');
 
-	$base_url = "http://localhost/clinic/";
+	$base_url = "https://canossahospitalpbh.in/";
 
 	if(!$conn){
 		die("Database Connection Failed: " . mysqli_connect_error());
